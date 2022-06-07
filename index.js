027363ef627ce12a9163e60ff7bb0a6b2f0c404d
Danh Dev <67886170+danhtran-dev@users.noreply.github.com>
2022-06-07 17:00:02 +0000

    add market data

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-const axios = require("axios");
 const fs = require("fs");
+const CoinGecko = require('coingecko-api');
 
-const getQuote = async () => {
-  try {
-    const { data } = await axios.get("https://quotes.rest/qod?language=en");
-    const quote = data.contents.quotes[0].quote;
-    const author = data.contents.quotes[0].author;
+const CoinGeckoClient = new CoinGecko();
 
-    console.log("new quote", `"${quote}"`);
+var checkMarketInfo = async() => {
+  let globalResponse = await CoinGeckoClient.global();
 
-    return {
-      quote,
-      author,
-    };
-  } catch (err) {
-    console.error(err.message);
-    return {};
+  if(globalResponse != null && globalResponse['code'] == 200)
+  {
+    const data = globalResponse['data']['data']
+    
+    const active_cryptocurrencies = data['active_cryptocurrencies']
+    const total_market_cap = data['total_market_cap']
+    const total_volume = data['total_volume']
+    const market_cap_percentage = data['market_cap_percentage']
+    const market_cap_change_percentage_24h_usd = data['market_cap_change_percentage_24h_usd']
+    const updateAt = data['updated_at']
+
+    // console.log(active_cryptocurrencies)
+    // console.log(total_market_cap)
+    // console.log(total_volume)
+    // console.log(market_cap_percentage)
+    // console.log(market_cap_change_percentage_24h_usd)
+    // console.log(updateAt)
+
+    const str1 = "Active Crypto: " + active_cryptocurrencies
+    const str2 = "% Market Cap: " + "BTC " + market_cap_percentage['btc'].toFixed(2) + "%" + " | ETH " + market_cap_percentage['eth'].toFixed(2) + "%" + " | BNB " + market_cap_percentage['bnb'].toFixed(2) + "%"
+    const str3 = "Total Market Cap Changed 24h: " + market_cap_change_percentage_24h_usd.toFixed(2) + "%"
+    const str4 = "Updated At: " + new Date().toLocaleString('en-GB')
+
+    console.log(str1)
+    console.log(str2)
+    console.log(str3)
+    console.log(str4)
+
+
+    var extractReadMe = fs.readFileSync('template.md').toString()
+    console.log(typeof extractReadMe)
+    extractReadMe = extractReadMe.replace('{CRYPTO_DATA_HERE}', str1 + "\n" + str2 + "\n" + str3 + "\n" + str4)
+    fs.writeFileSync("README.md", extractReadMe)
   }
 };
 
-const generate = async () => {
-  const { quote, author } = await getQuote();
-
-  if (!quote) return;
-
-  fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
-};
-
-generate();
+checkMarketInfo()
